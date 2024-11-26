c5175271336bb6975bdc6810716c4cc2fa79e50c
Golfsap <golf.sap@gmail.com>
2024-11-26 14:24:00 +0700

    Add password validation with live validation for each contraint

--- validation.js
+++ validation.js
@@ -4,23 +4,18 @@ const countryField = document.getElementById("country-field");
 const countryErrorField = document.getElementById("country-error");
 const zipField = document.getElementById("zip-field");
 const zipErrorField = document.getElementById("zip-error");
+const passwordField = document.getElementById("password-field");
 
 const validateEmail = (e) => {
   if (emailField.checkValidity()) {
     emailField.setCustomValidity("");
     console.log("valid");
     styleNoError("email");
-    // } else if (emailField.validity.valueMissing) {
-    //   emailErrorField.classList.remove("no-error");
-    //   emailField.setCustomValidity(" ❌ Please enter an email address");
-    //   // emailField.reportValidity();
-    //   console.log("invalid");
   } else {
     emailErrorField.classList.remove("no-error");
     emailErrorField.textContent = emailField.validationMessage;
     console.log("invalid");
     console.log(emailField.validity);
-    // emailField.reportValidity();
   }
 };
 
@@ -69,6 +64,87 @@ const styleNoError = (input) => {
   console.log(msgDiv);
 };
 
+const validateUppercase = () => {
+  const uppercaseConstraint = new RegExp("[A-Z]");
+  const uppercasePara = document.getElementById("uppercase");
+  if (uppercaseConstraint.test(passwordField.value)) {
+    // passwordField.setCustomValidity("");
+    uppercasePara.classList.add("no-error");
+    uppercasePara.textContent = "✅ At least one uppercase letter";
+    console.log("valid");
+    return true;
+  } else {
+    passwordField.setCustomValidity("At least one uppercase letter");
+    uppercasePara.textContent = passwordField.validationMessage;
+    uppercasePara.classList.remove("no-error");
+    return false;
+  }
+};
+
+const validateLowercase = () => {
+  const lowercaseConstraint = new RegExp("[a-z]");
+  const lowercasePara = document.getElementById("lowercase");
+  if (lowercaseConstraint.test(passwordField.value)) {
+    // passwordField.setCustomValidity("");
+    lowercasePara.classList.add("no-error");
+    lowercasePara.textContent = "✅ At least one lowercase letter";
+    console.log("valid");
+    return true;
+  } else {
+    passwordField.setCustomValidity("At least one lowercase letter");
+    lowercasePara.textContent = passwordField.validationMessage;
+    lowercasePara.classList.remove("no-error");
+    return false;
+  }
+};
+
+const validateDigit = () => {
+  const digitConstraint = new RegExp("[0-9]");
+  const digitPara = document.getElementById("digit");
+  if (digitConstraint.test(passwordField.value)) {
+    // passwordField.setCustomValidity("");
+    digitPara.classList.add("no-error");
+    digitPara.textContent = "✅ At least one digit";
+    console.log("valid");
+    return true;
+  } else {
+    passwordField.setCustomValidity("At least one digit");
+    digitPara.textContent = passwordField.validationMessage;
+    digitPara.classList.remove("no-error");
+    return false;
+  }
+};
+
+const validatePasswordLength = () => {
+  const charactersPara = document.getElementById("characters");
+  if (!passwordField.validity.tooShort) {
+    charactersPara.classList.add("no-error");
+    charactersPara.textContent = "✅ At least 8 characters long";
+    return true;
+  } else {
+    passwordField.setCustomValidity("At least 8 characters long");
+    charactersPara.textContent = passwordField.validationMessage;
+    charactersPara.classList.remove("no-error");
+    return false;
+  }
+};
+
+const validatePasswordUsingRegex = () => {
+  validateLowercase();
+  validateUppercase();
+  validateDigit();
+  validatePasswordLength();
+
+  if (
+    validateLowercase() &&
+    validateUppercase() &&
+    validateDigit() &&
+    validatePasswordLength()
+  ) {
+    passwordField.setCustomValidity("");
+  }
+};
+
 const postalRegex = {};
 
 async function fetchPostalCodeRegex() {
@@ -93,3 +169,4 @@ fetchPostalCodeRegex();
 emailField.addEventListener("input", validateEmail);
 countryField.addEventListener("input", validateCountry);
 zipField.addEventListener("input", validateZip);
+passwordField.addEventListener("input", validatePasswordUsingRegex);
