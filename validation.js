d6bcd9559eddd66b6fb44167fb482b2017915cc6
Golfsap <golf.sap@gmail.com>
2024-11-26 12:55:09 +0700

    Add zip code validation with imported regex function

--- validation.js
+++ validation.js
@@ -2,11 +2,12 @@ const emailField = document.getElementById("email-field");
 const emailErrorField = document.getElementById("email-error");
 const countryField = document.getElementById("country-field");
 const countryErrorField = document.getElementById("country-error");
+const zipField = document.getElementById("zip-field");
+const zipErrorField = document.getElementById("zip-error");
 
 const validateEmail = (e) => {
   if (emailField.checkValidity()) {
     emailField.setCustomValidity("");
-    // emailErrorField.classList.add("no-error");
     console.log("valid");
     styleNoError("email");
     // } else if (emailField.validity.valueMissing) {
@@ -35,6 +36,32 @@ const validateCountry = () => {
   }
 };
 
+const validateZip = () => {
+  if (!countryField.value) {
+    alert("Please select a country first");
+    return;
+  }
+  const countryRegex = postalRegex[countryField.value];
+  console.log(countryField.value);
+
+  // Build constraint checker
+  const constraint = new RegExp(countryRegex, "");
+  console.log(constraint);
+
+  // Check it
+  if (constraint.test(zipField.value)) {
+    zipField.setCustomValidity("");
+    styleNoError("zip");
+    console.log("valid");
+  } else {
+    zipField.setCustomValidity(
+      "Please match the following regex: " + constraint
+    );
+    zipErrorField.classList.remove("no-error");
+    zipErrorField.textContent = zipField.validationMessage;
+  }
+};
+
 const styleNoError = (input) => {
   const msgDiv = document.getElementById(`${input}-error`);
   msgDiv.classList.add("no-error");
@@ -42,6 +69,27 @@ const styleNoError = (input) => {
   console.log(msgDiv);
 };
 
+const postalRegex = {};
+
+async function fetchPostalCodeRegex() {
+  const response = await fetch("postalCodeData.xml");
+  const xmlString = await response.text();
+  const xmlDocument = new DOMParser().parseFromString(xmlString, "text/xml");
+  const postalCodeRegex = xmlDocument.getElementsByTagName("postCodeRegex");
+
+  for (let i = 0; i < postalCodeRegex.length; i++) {
+    const countryCode = postalCodeRegex[i].getAttribute("territoryId");
+    let regex = postalCodeRegex[i].textContent;
+    // Append "^" and "$"" to the regex to match exact string
+    regex = "^" + regex + "$";
+    postalRegex[countryCode] = regex;
+  }
+}
+
+// Populate the postal code regex object
+fetchPostalCodeRegex();
+
 // Event Listener
 emailField.addEventListener("input", validateEmail);
 countryField.addEventListener("input", validateCountry);
+zipField.addEventListener("input", validateZip);
